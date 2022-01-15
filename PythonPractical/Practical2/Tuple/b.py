5e93e4dfefb7481fa1f4054d55e1a7e701e22f66
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 17:00:37 +0530

    Create b.py

---
+++ PythonPractical/Practical2/Tuple/b.py
@@ -0,0 +1,5 @@
+#D21CE161 Parth Goswami
+#b. Write a Python program to create a tuple with numbers and print one item.
+tuple=(1,2,3,4,5,6,7,8)
+print("Tuple:",tuple)
+print("Tuple with one item:",tuple[1])
