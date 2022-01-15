a6416aad0963a26268f9d555eea86f55cae89ea6
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 15:54:41 +0530

    Update d.py

--- PythonPractical/Practical2/Dictionary/d.py
+++ PythonPractical/Practical2/Dictionary/d.py
@@ -1,9 +1,12 @@
 #D21CE161 Parth Goswami
 #d. Write a Python script to add a key to a dictionary.
 
+#dictionary
 Dictionary = {0:10,1:20}
 print(Dictionary)
+#getting key and value
 key=int(input("Enter key to add:"))
 value=int(input("Enter value to key:"))
+#using update fuction adding key and value
 Dictionary.update({key:value})
 print(Dictionary)
