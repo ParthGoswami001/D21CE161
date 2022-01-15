1a0049c1f9e0ec0c2a664b491556a4a2c699bab4
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 19:14:49 +0530

    commit

---
+++ PythonPractical/Practical2/Set/d.py
@@ -0,0 +1,11 @@
+#D21CE161 Parth Goswami
+#d. Write a Python program to find maximum and the minimum value in a set.
+#Create a set
+number= {1,3,4,5,6,8,9,7,4,45,56}
+print("Original set elements:")
+print(number)
+print(type(number))
+print("\nMaximum value of the set:")
+print(max(number))
+print("\nMinimum value of the set:")
+print(min(number))
