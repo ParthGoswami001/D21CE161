66680a1baa4327ba35e922632f761f3d44a0a5de
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 17:30:22 +0530

    Update e.py

---
+++ PythonPractical/Practical2/Tuple/e.py
@@ -0,0 +1,7 @@
+#D21CE161 Parth Goswami
+#e. Write a Python program to find the length of a tuple.
+#create a tuple
+tuplex = tuple("Programming in python")
+print(tuplex)
+#use the len() function to known the length of tuple
+print("length of tuple:",len(tuplex))
