c9d3f5742a55efed8470d7013a8fc3e30a818e96
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 19:35:00 +0530

    commit

--- PythonPractical/Practical2/Tuple/a.py
+++ PythonPractical/Practical2/Tuple/a.py
@@ -1,5 +1,5 @@
 #D21CE161 Parth Goswami
 #a. Write a Python program to create a tuple with different data types.
 
-tuple = ("String", False, 3.2, 1)
+tuple = ("String", False, 6.55, 1)
 print(tuple)
