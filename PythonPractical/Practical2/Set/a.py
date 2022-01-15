99d52195a06e03fb88547a4d5f7da85b40d16f22
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 17:43:39 +0530

    Update a.py

---
+++ PythonPractical/Practical2/Set/a.py
@@ -0,0 +1,11 @@
+#D21CE161 Parth Goswami
+#a. Write a Python program to add member(s) in a set and clear a set
+#A new empty set
+fruit_set = set()
+print(fruit_set)
+print("\nAdd single element:")
+fruit_set.add("Apple")
+print(fruit_set)
+print("\nAdd multiple items:")
+fruit_set.update(["Orange", "Grapes"])
+print(fruit_set)
