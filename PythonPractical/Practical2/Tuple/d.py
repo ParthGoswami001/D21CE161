ac7dec38bc761b034756665f164e245c608d8aa6
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 17:26:33 +0530

    Update d.py

---
+++ PythonPractical/Practical2/Tuple/d.py
@@ -0,0 +1,6 @@
+#D21CE161 Parth Goswami
+#d. Write a Python program to convert a tuple to a string.
+tuple = ('P', 'a', 'r', 't','h')
+print("Tuple:",tuple)
+str =  ''.join(tuple)
+print("String:",str)
