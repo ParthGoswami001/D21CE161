e2e52e7502c412688fb39d9a4504b7d3afe329ca
ParthGoswami001 <d21ce161@charusat.edu.in>
2022-01-15 17:49:23 +0530

    commit

---
+++ PythonPractical/Practical2/Set/b.py
@@ -0,0 +1,8 @@
+#D21CE161 Parth Goswami
+#b. Write a Python program to remove an item from a set if it is present in the set.
+number_set = set([5, 6, 7, 8, 9])
+print("Original set:")
+print(number_set)
+number_set.pop()
+print("\nAfter removing the first element from the said set:")
+print(number_set)
