edbf425e46f9a9ee03f9aaf32db31bc96dd8c755
DivineCodes <97752096+ParthGoswami001@users.noreply.github.com>
2022-03-31 22:54:58 +0530

    Add files via upload

---
+++ PythonPractical/8.py
@@ -0,0 +1,16 @@
+stack=[]
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print ('install stack')
+
+print (stack)
+
+print ('\n element pop from stack')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print ('\n stack after element are poped:')
+print(stack)
